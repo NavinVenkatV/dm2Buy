b919529a4141442bd7a92d5952d62b8796008387
NavinVenkatV <vnavinvenkat@gmail.com>
2025-05-16 23:42:35 +0530

    socket_minor_corr

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -13,14 +13,18 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
+const allowedUrls = ['https://dm2-buy.vercel.app', 'http://localhost:3000'];
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "https://dm2-buy.vercel.app/",
-        methods: ["GET", "POST", "PUT"]
+        origin: allowedUrls,
+        methods: ["GET", "POST", "PUT", "OPTIONS"]
     }
 });
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: allowedUrls,
+    credentials: true
+}));
 io.on("connection", (socket) => {
     console.log("New user connected:", socket.id);
     socket.on("join-room", (roomId) => {
@@ -36,5 +40,7 @@ io.on("connection", (socket) => {
 });
 app.use("/api/user", userRoutes_1.default);
 app.use("/api/snippet", snippetRoutes_1.default);
-server.listen(process.env.PORT || 3000, () => console.log("Server running on port "));
-console.log(process.env.PORT);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
